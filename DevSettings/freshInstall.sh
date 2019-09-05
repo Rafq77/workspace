528882522bc9f73bfc36163731d3ce2382ac0346
Rafq77 <sokol-77@wp.pl>
2019-09-05 13:08:50 +0200

    Added other libs needed to yocto build for beaglebone-green.

--- DevSettings/freshInstall.sh
+++ DevSettings/freshInstall.sh
@@ -32,7 +32,7 @@ else  # TODO elif [ uname -a | grep Ubuntu ]
         sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
         build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
         xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
-        xterm
+        xterm npm jq docker.io
     else
         echo "${PRFX} Input was: $confirm, additional packages will not be installed."
     fi
